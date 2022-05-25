71421c779ae73f11f7ef0d32a396f9024a38cfc6
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-05-25 17:28:42 +0545

    next button eventlistner added

---
+++ Carousel/js/carousel.js
@@ -0,0 +1,20 @@
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel__button--right");
+const prevButton = document.querySelector(".carousel__button--left");
+
+const dotsNav = document.querySelector(".carousel__nav");
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+slides.forEach((slide, index) => {
+	slide.style.left = slideWidth * index + "px";
+});
+
+nextButton.addEventListener("click", function () {
+	const currentSlide = track.querySelector(".current-slide");
+	const nextSlide = currentSlide.nextElementSibling;
+	const amountToMove = nextSlide.style.left;
+	track.style.transform = "translateX(-" + amountToMove + ")";
+});
