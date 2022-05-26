89bf86eb83e68c220c9920f8b94f729d5a2ca007
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-05-26 14:40:42 +0545

    canculator layout completed

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,1 +1,22 @@
-let screen = document.querySelector("#screen");
+let screen = document.getElementById("screen");
+let screenValue = " ";
+buttons = document.querySelectorAll("button");
+for (item of buttons) {
+	item.addEventListener("click", function (e) {
+		buttonText = e.target.innerText;
+		console.log(buttonText);
+		if (buttonText == "X") {
+			buttonText = "*";
+			screenValue += buttonText;
+			screen.value = screenValue;
+		} else if (buttonText == "C") {
+			screenValue = "";
+			screen.value = screenValue;
+		} else if (buttonText == "=") {
+			screen.value = eval(screenValue);
+		} else {
+			screenValue += buttonText;
+			screen.value = screenValue;
+		}
+	});
+}
