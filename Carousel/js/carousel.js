c35ac070d7ca25be5e46321ede3e5653441c6e37
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-05-30 13:23:00 +0545

    class implemented in slider

--- Carousel/js/carousel.js
+++ Carousel/js/carousel.js
@@ -2,30 +2,72 @@ let x = 0;
 
 const track = document.querySelector(".carousel__track");
 const body = document.querySelector(".carousel");
-const slides = Array.from(track.children);
+
 const nextButton = document.querySelector(".carousel__button--right");
 const prevButton = document.querySelector(".carousel__button--left");
 const dotsNav = document.querySelector(".carousel__nav");
 const currentDot = dotsNav.querySelector(".carousel__indicator--current-slide");
+
+//carousel class//
+class Carousel {
+	constructor() {}
+	createCarousel(image) {
+		console.log(image);
+		const li = document.createElement("li");
+		li.classList.add("carousel__slide");
+		const photo = document.createElement("img");
+		photo.src = image;
+		photo.classList.add("carousel__slide-image");
+		li.appendChild(photo);
+		track.appendChild(li);
+	}
+}
+
+//input images //
+const slider1 = new Carousel();
+slider1.createCarousel("./assets/slide1.jpg");
+const slider2 = new Carousel();
+slider2.createCarousel("./assets/slide2.jpg");
+const slider3 = new Carousel();
+slider3.createCarousel("./assets/slide3.jpg");
+const slider4 = new Carousel();
+slider4.createCarousel("./assets/slide4.jpg");
+
+//looping for navigation dots//
+
+let slides = Array.from(track.children);
+for (let j = 0; j < slides.length; j++) {
+	const dot = document.createElement("button");
+	if (j == 0) {
+		dot.classList.add(
+			"carousel__indicator",
+			"carousel__indicator--current-slide",
+		);
+	} else {
+		dot.classList.add("carousel__indicator");
+	}
+	dotsNav.appendChild(dot);
+}
 const dots = Array.from(dotsNav.children);
+
 let targetDot = dots[0];
 const slideWidth = slides[0].getBoundingClientRect().width;
 let a = 0;
 let myInterval;
 
-// body.addEventListener("click", function () {
-// 	clearInterval(myInterval);
-// });
+//for pausing while hovering
 body.addEventListener("mouseenter", function () {
 	clearInterval(myInterval);
 });
 
+//for playing back after mouseleave
 body.addEventListener("mouseleave", function () {
 	myInterval = setInterval(function () {
 		document.querySelector(".carousel__button--right").click();
 	}, 4000);
 });
 
+//setting slide horizontally
 slides.forEach((slide, index) => {
 	slide.style.left = slideWidth * index + "px";
 });
@@ -72,6 +114,7 @@ prevButton.addEventListener("click", function () {
 	targetDot = dots[x / 100];
 	targetDot.classList.add("carousel__indicator--current-slide");
 });
+
 dotsNav.addEventListener("click", function (e) {
 	const targetDot = e.target.closest("button");
 	const currentDot = dotsNav.querySelector(
