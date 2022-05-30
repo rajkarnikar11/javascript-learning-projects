9592ea3aa5b62f58b03837c61c162b437a0ac041
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-05-30 16:36:29 +0545

    title updated

--- Carousel/js/carousel.js
+++ Carousel/js/carousel.js
@@ -1,5 +1,4 @@
-let x = 0;
-
+let myInterval;
 const track = document.querySelector(".carousel__track");
 const body = document.querySelector(".carousel");
 
@@ -10,122 +9,141 @@ const currentDot = dotsNav.querySelector(".carousel__indicator--current-slide");
 
 //carousel class//
 class Carousel {
-	constructor() {}
-	createCarousel(image) {
-		console.log(image);
+	constructor(image) {
+		let x = 0;
+		let j = 0;
+		let a = 0;
+		this.image = image;
+		console.log(this.image);
 		const li = document.createElement("li");
 		li.classList.add("carousel__slide");
 		const photo = document.createElement("img");
-		photo.src = image;
+		photo.src = this.image;
 		photo.classList.add("carousel__slide-image");
 		li.appendChild(photo);
 		track.appendChild(li);
 	}
+	autoclick() {
+		let currentDot = this.currentDot;
+		let x = 0;
+		let slides = Array.from(track.children);
+
+		let a = 0;
+		const dot = document.createElement("button");
+		for (let j = 0; j < slides.length; j++) {
+			if (j == 0) {
+				dot.classList.add(
+					"carousel__indicator",
+					"carousel__indicator--current-slide",
+				);
+			} else {
+				dot.classList.add("carousel__indicator");
+			}
+			dotsNav.appendChild(dot);
+		}
+		const dots = Array.from(dotsNav.children);
+		var targetDot = dots[0];
+
+		const slideWidth = slides[0].getBoundingClientRect().width;
+		//setting slide horizontally
+		slides.forEach((slide, index) => {
+			slide.style.left = slideWidth * index + "px";
+		});
+		myInterval = setInterval(function () {
+			document.querySelector(".carousel__button--right").click();
+		}, 4000);
+
+		nextButton.addEventListener("click", function () {
+			let currentDot = dotsNav.querySelector(
+				".carousel__indicator--current-slide",
+			);
+			this.currentDot = currentDot;
+
+			currentDot.classList.remove("carousel__indicator--current-slide");
+
+			if (x < (slides.length - 1) * 100) {
+				x = x + 100;
+				track.style.transform = "translateX(-" + x + "%)";
+			} else {
+				track.style.transform = "translateX(0px)";
+				x = 0;
+			}
+			targetDot.classList.remove("carousel__indicator--current-slide");
+
+			targetDot = dots[x / 100];
+			targetDot.classList.add("carousel__indicator--current-slide");
+		});
+		body.addEventListener("mouseenter", function () {
+			clearInterval(myInterval);
+		});
+
+		//for playing back after mouseleave
+		body.addEventListener("mouseleave", function () {
+			myInterval = setInterval(function () {
+				document.querySelector(".carousel__button--right").click();
+			}, 4000);
+		});
+
+		prevButton.addEventListener("click", function () {
+			const currentDot = dotsNav.querySelector(
+				".carousel__indicator--current-slide",
+			);
+
+			currentDot.classList.remove("carousel__indicator--current-slide");
+
+			if (x != 0) {
+				x = x - 100;
+				track.style.transform = "translateX(-" + x + "%)";
+			} else {
+				track.style.transform =
+					"translateX(-" + (slides.length - 1) * 100 + "%)";
+				x = (slides.length - 1) * 100;
+			}
+			targetDot.classList.remove("carousel__indicator--current-slide");
+
+			targetDot = dots[x / 100];
+			targetDot.classList.add("carousel__indicator--current-slide");
+		});
+
+		dotsNav.addEventListener("click", function (e) {
+			const targetDot = e.target.closest("button");
+			const currentDot = dotsNav.querySelector(
+				".carousel__indicator--current-slide",
+			);
+
+			if (!targetDot) return;
+			const targetIndex = dots.findIndex((dot) => dot === targetDot);
+			track.style.transform = "translateX(-" + targetIndex * 100 + "%)";
+			x = targetIndex * 100;
+			currentDot.classList.remove("carousel__indicator--current-slide");
+
+			targetDot.classList.add("carousel__indicator--current-slide");
+		});
+	}
 }
 
 //input images //
-const slider1 = new Carousel();
-slider1.createCarousel("./assets/slide1.jpg");
-const slider2 = new Carousel();
-slider2.createCarousel("./assets/slide2.jpg");
-const slider3 = new Carousel();
-slider3.createCarousel("./assets/slide3.jpg");
-const slider4 = new Carousel();
-slider4.createCarousel("./assets/slide4.jpg");
+const slider1 = new Carousel("./assets/slide1.jpg");
+const slider2 = new Carousel("./assets/slide2.jpg");
+const slider3 = new Carousel("./assets/slide3.jpg");
+const slider4 = new Carousel("./assets/slide4.jpg");
+slider4.autoclick();
 
 //looping for navigation dots//
 
-let slides = Array.from(track.children);
-for (let j = 0; j < slides.length; j++) {
-	const dot = document.createElement("button");
-	if (j == 0) {
-		dot.classList.add(
-			"carousel__indicator",
-			"carousel__indicator--current-slide",
-		);
-	} else {
-		dot.classList.add("carousel__indicator");
-	}
-	dotsNav.appendChild(dot);
-}
-const dots = Array.from(dotsNav.children);
-
-let targetDot = dots[0];
-const slideWidth = slides[0].getBoundingClientRect().width;
-let a = 0;
-let myInterval;
+// let slides = Array.from(track.children);
+// for (let j = 0; j < slides.length; j++) {
+// 	const dot = document.createElement("button");
+// 	if (j == 0) {
+// 		dot.classList.add(
+// 			"carousel__indicator",
+// 			"carousel__indicator--current-slide",
+// 		);
+// 	} else {
+// 		dot.classList.add("carousel__indicator");
+// 	}
+// 	dotsNav.appendChild(dot);
+// }
+// const dots = Array.from(dotsNav.children);
 
 //for pausing while hovering
-body.addEventListener("mouseenter", function () {
-	clearInterval(myInterval);
-});
-
-//for playing back after mouseleave
-body.addEventListener("mouseleave", function () {
-	myInterval = setInterval(function () {
-		document.querySelector(".carousel__button--right").click();
-	}, 4000);
-});
-
-//setting slide horizontally
-slides.forEach((slide, index) => {
-	slide.style.left = slideWidth * index + "px";
-});
-myInterval = setInterval(function () {
-	document.querySelector(".carousel__button--right").click();
-}, 4000);
-
-nextButton.addEventListener("click", function () {
-	const currentDot = dotsNav.querySelector(
-		".carousel__indicator--current-slide",
-	);
-
-	currentDot.classList.remove("carousel__indicator--current-slide");
-
-	if (x < (slides.length - 1) * 100) {
-		x = x + 100;
-		track.style.transform = "translateX(-" + x + "%)";
-	} else {
-		track.style.transform = "translateX(0px)";
-		x = 0;
-	}
-	targetDot.classList.remove("carousel__indicator--current-slide");
-
-	targetDot = dots[x / 100];
-	targetDot.classList.add("carousel__indicator--current-slide");
-});
-prevButton.addEventListener("click", function () {
-	const currentDot = dotsNav.querySelector(
-		".carousel__indicator--current-slide",
-	);
-
-	currentDot.classList.remove("carousel__indicator--current-slide");
-
-	if (x != 0) {
-		x = x - 100;
-		track.style.transform = "translateX(-" + x + "%)";
-	} else {
-		track.style.transform =
-			"translateX(-" + (slides.length - 1) * 100 + "%)";
-		x = (slides.length - 1) * 100;
-	}
-	targetDot.classList.remove("carousel__indicator--current-slide");
-
-	targetDot = dots[x / 100];
-	targetDot.classList.add("carousel__indicator--current-slide");
-});
-
-dotsNav.addEventListener("click", function (e) {
-	const targetDot = e.target.closest("button");
-	const currentDot = dotsNav.querySelector(
-		".carousel__indicator--current-slide",
-	);
-
-	if (!targetDot) return;
-	const targetIndex = dots.findIndex((dot) => dot === targetDot);
-	track.style.transform = "translateX(-" + targetIndex * 100 + "%)";
-	x = targetIndex * 100;
-	currentDot.classList.remove("carousel__indicator--current-slide");
-
-	targetDot.classList.add("carousel__indicator--current-slide");
-});
