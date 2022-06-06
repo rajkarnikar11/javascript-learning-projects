ba8f964e2b4b060b6be09529d3e8bb4e34e56d8e
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-06-06 10:28:15 +0545

    button implemented

--- quotes/fetch.js
+++ quotes/fetch.js
@@ -1,22 +1,26 @@
 const body = document.querySelector(".quotes__container");
+const button = document.querySelector(".quotes__button");
 // const author = document.querySelector(".card__author");
+function displayQuote() {
+	fetch("https://type.fit/api/quotes")
+		.then((res) => res.json())
+		.then((data) => {
+			data.forEach((quotes) => {
+				const card = document.createElement("div");
+				const quote = document.createElement("p");
+				quote.innerText = quotes.text;
+				quote.classList.add("card__description");
+				card.appendChild(quote);
 
-fetch("https://type.fit/api/quotes")
-	.then((res) => res.json())
-	.then((data) => {
-		data.forEach((quotes) => {
-			const card = document.createElement("div");
-			const quote = document.createElement("p");
-			quote.innerText = quotes.text;
-			quote.classList.add("card__description");
-			card.appendChild(quote);
+				const author = document.createElement("p");
+				author.innerText = `~ ${quotes.author}`;
+				author.classList.add("card__author");
 
-			const author = document.createElement("p");
-			author.innerText = `~ ${quotes.author}`;
-			author.classList.add("card__author");
-
-			card.appendChild(author);
-			body.appendChild(card);
-			card.classList.add("card__container");
+				card.appendChild(author);
+				body.appendChild(card);
+				card.classList.add("card__container");
+			});
 		});
-	});
+}
+
+document.addEventListener("click", displayQuote);
